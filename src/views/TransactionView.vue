e994e84c542423fe8930ab23efa9d88b2b6bab19
sunpeijia <1311107143@qq.com>
2018-03-04 23:25:49 +0800

    no message

--- src/views/TransactionView.vue
+++ src/views/TransactionView.vue
@@ -4,7 +4,6 @@
     <table id="payouts" class="table table-striped table-sm table-bordered" cellspacing="0" width="100%">
       <thead>
             <tr>
-                <th>Estate Name</th>
                 <th>New Owner</th>
                 <th>Purchase Price</th>
                 <th>Timestamp</th>
@@ -12,10 +11,9 @@
         </thead>
         <tbody>
           <tr v-for="data in datas">
-              <td>Saint Basil&#039;s Cathedral</td>
-              <td>F7714F4484</td>
-              <td>1.5778 ETH</td>
-              <td>1 day ago</td>
+              <td>{{data.owner}}</td>
+              <td>{{data.price}} ETH</td>
+              <td>{{data.time}}</td>
           </tr>
         </tbody>
     </table>
@@ -32,10 +30,21 @@ export default {
     }
   },
   mounted: function() {
-    // this.$http.get('').then(function(response) {
-    //       this.datas = response.body.datas;
-    // });
-    this.datas = ["","","",""];
+    this.$http.get('https://api.etherscan.io/api?module=account&address=0x8d12a197cb00d4747a1fe03395095ce2a5cc6819&offset=100&page=1&sort=asc&apikey=FYFY6687BY19QA9TEF7NU2NAHQW6AFS5AN&action=txlist').then((response) => {
+      var result = JSON.parse(response.bodyText).result
+      console.log(result)
+      var res = [];
+      for (var i in result) {
+        var data = new Array();
+        data["owner"] = web3.sha3(result[i]["to"]).substr(2,8);
+        data["price"] = parseInt(result[i]["gasPrice"]) * parseInt(result[i]["gasUsed"]) / Math.pow(10,18);
+        data["time"] = new Date(parseInt(result[i]["timeStamp"]) * 1000).toLocaleString();
+        res[i] = data;
+      }
+      this.datas = res;
+    }, (response) => {
+    });
+    // this.datas = ["","","",""];
   }
 
 };
