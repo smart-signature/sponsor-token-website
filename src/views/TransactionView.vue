c7d87de6761a048d5165e1484df3b52f4ca401f9
sunpeijia <1311107143@qq.com>
2018-03-06 21:50:55 +0800

    交易list样式并跳转tx

--- src/views/TransactionView.vue
+++ src/views/TransactionView.vue
@@ -10,7 +10,7 @@
             </tr>
         </thead>
         <tbody>
-          <tr v-for="data in datas">
+          <tr class="txs" v-for="data in datas" v-on:click="clickList(data)" v-on:mouseover="overList()">
               <td>{{data.owner}}</td>
               <td>{{data.price}} ETH</td>
               <td>{{data.time}}</td>
@@ -29,6 +29,16 @@ export default {
       datas:[]
     }
   },
+  methods: {
+    clickList(data) {
+        var w = window.open('about:blank');
+        var url = "https://etherscan.io/tx/" + data.hash;
+        w.location.href = url;
+    },
+    overList() {
+      console.log("dsafsda")
+    }
+  },
   mounted: function() {
     this.$http.get('https://api.etherscan.io/api?module=account&address=0x8d12a197cb00d4747a1fe03395095ce2a5cc6819&offset=100&page=1&sort=asc&apikey=FYFY6687BY19QA9TEF7NU2NAHQW6AFS5AN&action=txlist').then((response) => {
       var result = JSON.parse(response.bodyText).result
@@ -39,6 +49,7 @@ export default {
         data["owner"] = web3.sha3(result[i]["to"]).substr(2,8);
         data["price"] = parseInt(result[i]["gasPrice"]) * parseInt(result[i]["gasUsed"]) / Math.pow(10,18);
         data["time"] = new Date(parseInt(result[i]["timeStamp"]) * 1000).toLocaleString();
+        data["hash"] = result[i]["hash"];
         res[i] = data;
       }
       this.datas = res;
@@ -53,5 +64,14 @@ export default {
 <style scoped>
 .title {
   text-align: center;
+}
+.txs { 
+  background: LightGray; 
+}
+.txs:nth-child(odd) { 
+  background: white; 
+}
+.txs:hover { 
+  background: gray; 
 }
 </style>
